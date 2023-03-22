6fdeab1f6236d7cc82664e65b91e084d654fe9b7
Anonymous893 <124325801+Anonymous893@users.noreply.github.com>
2023-03-22 11:23:45 -0400

    Update BattlePlayer.java

--- Battleship/BattlePlayer.java
+++ Battleship/BattlePlayer.java
@@ -8,41 +8,27 @@ public class BattlePlayer {
 	private BattleBoard board;
 
 	public BattlePlayer(String n) {
-
 		name = n;
 		board = new BattleBoard();
-	
 	}
 
-
 	public boolean checkInput(String input) {
-
 		return board.checkInput(input);
-
 	}
 
 	public void update() {
-
 		board.update();
-
 	}
 
 	public boolean gameOver() {
-
 		return board.gameOver(); 
-
 	}
 
 	public String getName() {
-
 		return name;
-
 	}
 
 	public void printBoard() {
-
 		board.print();
-
 	}
-
 }
